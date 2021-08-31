8491317fbd52d7f2fb4b2dab25c619dfa382c18e
Minh Ha <minh.ha@minogames.com>
2021-08-31 11:31:09 -0700

    Revert "Fix more compilation errors"
    This reverts commit 011c8a8ed5fcc2e418a78e699e6a2ffac4367b6b.

--- src/openfl/utils/AssetLibrary.hx
+++ src/openfl/utils/AssetLibrary.hx
@@ -1,21 +1,20 @@
 package openfl.utils;
 
-import openfl.display.MovieClip;
-import lime.app.Future;
 
-#if lime
+import lime.app.Future;
 import lime.graphics.Image;
 import lime.media.AudioBuffer;
 import lime.text.Font;
-import lime.utils.AssetLibrary as LimeAssetLibrary;
+import lime.utils.AssetLibrary in LimeAssetLibrary;
 import lime.utils.AssetManifest;
 import lime.utils.Bytes;
-#end
+import openfl.display.MovieClip;
 
 #if !openfl_debug
 @:fileXml('tags="haxe,release"')
 @:noDebug
 #end
+
 
 @:dox(hide) class AssetLibrary extends LimeAssetLibrary {
 	
@@ -303,17 +302,13 @@ import lime.utils.Bytes;
 	}
 	
 	
-   public static function loadFromBytes (bytes:ByteArray, rootPath:String = null):#if (java && lime) Future<LimeAssetLibrary> #else Future<AssetLibrary> #end {
+	public static function loadFromBytes (bytes:ByteArray, rootPath:String = null):#if java Future<LimeAssetLibrary> #else Future<AssetLibrary> #end {
 		
-        #if lime
 		return AssetManifest.loadFromBytes (bytes, rootPath).then (function (manifest) {
 			
 			return loadFromManifest (manifest);
 			
 		});
-		#else
-		return cast Future.withValue (null);
-		#end
 		
 	}
 	
